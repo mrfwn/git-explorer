b58fc5eee12082260f1be7d7f0c0b20ae673dc6e
Mário Wessen <mariowessen@gmail.com>
2020-07-24 09:31:18 -0300

    feature: style of the dashboard, installed react icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,62 @@
 import React from 'react';
-import { Title } from './styles';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/13460706?s=460&u=8fca030bfb6aafdb098d31d9521a10cdf18dd48c&v=4"
+          alt="Mário Wessen"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native form! </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/13460706?s=460&u=8fca030bfb6aafdb098d31d9521a10cdf18dd48c&v=4"
+          alt="Mário Wessen"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native form! </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/13460706?s=460&u=8fca030bfb6aafdb098d31d9521a10cdf18dd48c&v=4"
+          alt="Mário Wessen"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native form! </p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
